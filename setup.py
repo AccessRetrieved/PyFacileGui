0e328b445360c0ff104dce1d39ef8cdf8af83f7d
Jerry <56012931+AccessRetrieved@users.noreply.github.com>
2020-10-04 12:17:59 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from distutils.core import setup
 setup(
-  name = 'PyGui',         # How you named your package folder (MyLib)
-  packages = ['PyGui'],   # Chose the same as "name"
+  name = 'PyFacileGui',         # How you named your package folder (MyLib)
+  packages = ['PyFacileGui'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Simpler life with most modules combined.',   # Give a short description about your library
   author = 'Jerry Hu',                   # Type in your name
   author_email = 'bestfit.jerry.wu@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/AccessRetrieved/PyGui',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/AccessRetrieved/PyGui/archive/0.1.tar.gz',    # I explain this later on
-  keywords = ['pygui', 'PyGui', 'python', 'Python', 'easy', 'combined', 'modules'],   # Keywords that define your package best
+  url = 'https://github.com/AccessRetrieved/PyFacileGui',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/AccessRetrieved/PyFacileGui/archive/0.1.tar.gz',    # I explain this later on
+  keywords = ['pygui', 'PyGui', 'python', 'Python', 'easy', 'combined', 'modules', 'pyfacilegui', 'PyFacileGui', 'Pyfacilegui'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pyautogui',
           'keyboard',
           'yagmail',
-          'touchid',
           'tkinter',
           'rumps'
       ],
