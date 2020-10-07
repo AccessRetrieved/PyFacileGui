c272225d9d9d3b3006dd578d24a5ffdca2d7c54e
Jerry <56012931+AccessRetrieved@users.noreply.github.com>
2020-10-07 10:52:47 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from distutils.core import setup
 setup(
-  name = 'PyFacileGui',         # How you named your package folder (MyLib)
-  packages = ['PyFacileGui'],   # Chose the same as "name"
-  version = '0.4',      # Start with a small number and increase it with every change you make
+  name = 'PyEasyGUi',         # How you named your package folder (MyLib)
+  packages = ['PyEasyGui'],   # Chose the same as "name"
+  version = '0.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Simpler life with most modules combined.',   # Give a short description about your library
   author = 'Jerry Hu',                   # Type in your name
   author_email = 'bestfit.jerry.wu@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/AccessRetrieved/PyFacileGui',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/AccessRetrieved/PyFacileGui/archive/0.4.tar.gz',    # I explain this later on
-  keywords = ['pygui', 'PyGui', 'python', 'Python', 'easy', 'combined', 'modules', 'pyfacilegui', 'PyFacileGui', 'Pyfacilegui'],   # Keywords that define your package best
+  keywords = ['pygui', 'PyGui', 'python', 'Python', 'easy', 'combined', 'modules', 'pyfacilegui', 'PyFacileGui', 'Pyfacilegui', 'PyEasyGui', 'pyeasygui'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pyautogui',
           'keyboard',
-          'yagmail',
           'rumps'
       ],
   classifiers=[
@@ -23,6 +22,7 @@ setup(
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3.6',    #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8'
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9'
   ],
 )
