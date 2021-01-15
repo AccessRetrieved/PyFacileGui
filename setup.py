d7ed34231d3eab6d2c2576be8efd91be2c5d4db8
Jerry <56012931+AccessRetrieved@users.noreply.github.com>
2021-01-15 12:52:46 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'PyEasyGUi',         # How you named your package folder (MyLib)
-  packages = ['PyEasyGui'],   # Chose the same as "name"
+  name = 'PyFacileGUi',         # How you named your package folder (MyLib)
+  packages = ['PyFacileGui'],   # Chose the same as "name"
   version = '0.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Simpler life with most modules combined.',   # Give a short description about your library
